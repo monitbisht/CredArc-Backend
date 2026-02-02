145c3086989ba2f7bde3d494b5b230b9eebc049f
Monit <monitbisht04@gmail.com>
2026-02-03 02:29:59 +0530

    Finalize account creation DTO and request validation

--- src/main/java/com/credarc/credarc/dto/AccountCreationResponse.java
+++ src/main/java/com/credarc/credarc/dto/AccountCreationResponse.java
@@ -3,6 +3,9 @@ package com.credarc.credarc.dto;
 public class AccountCreationResponse {
     private String confirmationMessage;
 
+    public String getConfirmationMessage() {
+        return confirmationMessage;
+    }
 
     public void setConfirmationMessage(String message) {
         this.confirmationMessage = message;
