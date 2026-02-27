437d2765f9ad0049f77c8ceca474469985987f01
Monit <monitbisht04@gmail.com>
2026-02-27 12:35:54 +0530

    Added email and mobile existence checks to UserRepository

--- src/main/java/com/credarc/credarc/repository/UserRepository.java
+++ src/main/java/com/credarc/credarc/repository/UserRepository.java
@@ -8,5 +8,7 @@ import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByMobile(String mobile);
+
 }
