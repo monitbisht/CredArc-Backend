1b0e72ea7fb0789e0dca0f815b660008efdf4261
monitbisht <monitbisht04@gmail.com>
2026-08-12 14:26:52 +0530

    Set up Redis integration for local profile

--- src/main/java/com/credarc/credarc/CredarcApplication.java
+++ src/main/java/com/credarc/credarc/CredarcApplication.java
@@ -2,8 +2,10 @@ package com.credarc.credarc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CredarcApplication {
 
 	public static void main(String[] args) {
